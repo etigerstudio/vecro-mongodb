a1b10e0fc5ee7ff087780422ca8b6d702d1fb7f0
etigerstudio <etigerstudio@gmail.com>
2021-11-05 15:32:10 +0800

    fix: skip calling endpoint when call list is empty.
    refactor:
     - extract environment variables parse logic.
     - rename “callee” to “call”.

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/go-kit/kit/endpoint"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
-
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/go-kit/kit/log"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
@@ -20,34 +19,23 @@ func main() {
 	const subsystemEnvKey = "BEN_SUBSYSTEM"
 	const serviceTypeEnvKey = "BEN_SERVICE_TYPE"
 	const listenAddressEnvKey = "BEN_LISTEN_ADDRESS"
-	const calleeEnvKey = "BEN_CALLS"
-	const calleeSeparator = " "
+	const callEnvKey = "BEN_CALLS"
+	const callSeparator = " "
 
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "caller", log.DefaultCaller)
 
-	serviceTypeStr, exists := os.LookupEnv(serviceTypeEnvKey)
+	serviceTypeStr, _ := getEnvString(serviceTypeEnvKey, string(vanilla))
 	serviceType := ServiceType(serviceTypeStr)
-	if !exists {
-		serviceType = vanilla
-	}
+
 	logger.Log("service_type", serviceType)
 
-	listenAddress, exists := os.LookupEnv(listenAddressEnvKey)
-	if !exists {
-		listenAddress = ":8080"
-	}
+	listenAddress, _ := getEnvString(listenAddressEnvKey, ":8080")
 	logger.Log("listen_address", listenAddress)
 
-	subsystem := os.Getenv(subsystemEnvKey)
-	if subsystem == "" {
-		subsystem = "subsystem"
-	}
-	name := os.Getenv(nameEnvKey)
-	if name == "" {
-		name = "name"
-	}
+	subsystem, _ := getEnvString(subsystemEnvKey, "subsystem")
+	name, _ := getEnvString(nameEnvKey, "name")
 	logger.Log("name", name, "subsystem", subsystem)
 
 	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
@@ -64,22 +52,27 @@ func main() {
 		Buckets:     []float64{.001, .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
 	}, nil)
 
-	// Create callee endpoint list from the environment variable
+	// Create call endpoint list from the environment variable
 	var calls []endpoint.Endpoint
-	calleeList := os.Getenv(calleeEnvKey)
-	logger.Log("calls", calleeList)
-	for _, calleeStr := range strings.Split(calleeList, calleeSeparator) {
-		callURL, err := url.Parse(calleeStr)
-		if err != nil {
-			panic(err)
+	callList, exists := getEnvString(callEnvKey, "")
+	if exists {
+		logger.Log("calls", callList)
+
+		for _, callStr := range strings.Split(callList, callSeparator) {
+			callURL, err := url.Parse(callStr)
+			if err != nil {
+				panic(err)
+			}
+			callEndpoint := httptransport.NewClient(
+				"GET",
+				callURL,
+				encodeRequest,
+				decodeBaseResponse,
+			).Endpoint()
+			calls = append(calls, callEndpoint)
 		}
-		calleeEndpoint := httptransport.NewClient(
-			"GET",
-			callURL,
-			encodeRequest,
-			decodeBaseResponse,
-		).Endpoint()
-		calls = append(calls, calleeEndpoint)
+	} else {
+		logger.Log("calls", "[empty call list]")
 	}
 
 	var svc BaseService
@@ -97,4 +90,15 @@ func main() {
 	http.Handle("/metrics", promhttp.Handler())
 	logger.Log("msg", "HTTP", "addr", listenAddress)
 	logger.Log("err", http.ListenAndServe(listenAddress, nil))
+}
+
+// Get an environment variable by name
+// which is replaced by default value if it's empty
+func getEnvString(key string, value string) (string, bool) {
+	v := os.Getenv(key)
+	if v == "" {
+		return value, false
+	}
+
+	return v, true
 }
