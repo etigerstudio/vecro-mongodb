7fb5b23a7ae4bc20b629f2978ee135d9b665f263
etigerstudio <etigerstudio@gmail.com>
2021-11-05 15:33:12 +0800

    chore: image build script now supports multi-node clusters.

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ben-base . &&
-docker build -t ben-base:v1 .
+# docker build -t ben-base:v1 .
+minikube -p n1 image build -t ben-base:v1 .
