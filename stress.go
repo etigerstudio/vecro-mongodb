c38ace32c0dc65c83b3e6662b85e7ab7493e73ed
etigerstudio <etigerstudio@gmail.com>
2022-05-07 02:44:24 +0800

    refactor(stress): adopt built-in cpu & io stress logic.
    stress-ng is deprecated for being too slow to launch.

--- stress.go
+++ stress.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"math"
 	"math/rand"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	stressNgName = "./stress-ng"
-	cpuOpsBase = 3
-	ioOpsBase = 4
+	cpuOpsBase = 16384 * 2 // stress-ng use 16384 as a minimal sqrt(rand()) unit
+	ioOpsBase = 64 // 64 times * 1 KiB = 64 KiB
 )
 
 // Main stressing entry
@@ -39,21 +41,64 @@ func delay(delayTime int, delayJitter int) {
 }
 
 func cpuStress(cpuLoad int) {
-	// Approximately 3,000 ops per sec => 3 ops per 1ms
+	// Approximately 16,384 * 2 ops per 1ms
 	// on Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20GHz
-	execCommand(stressNgName,
-		"--cpu", "1",
-		"--cpu-ops", strconv.Itoa(cpuOpsBase * cpuLoad),
-		"--cpu-method", "sqrt")
-	log.Printf("cpu load amount: %d\n", cpuLoad)
+	for i := 0; i < cpuOpsBase * cpuLoad; i++ {
+		math.Sqrt(rand.Float64())
+	}
+	log.Printf("cpu load amount: %d, total sqrt(rand()): %d\n", cpuLoad, cpuOpsBase * cpuLoad)
 }
 
+// FIXME: Better file specific error handling in this function
 func ioStress(ioLoad int) {
-	// Approximately 4,000 ops per sec => 4 ops per 1ms
-	// on Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20GHz
-	execCommand(stressNgName,
-		"--hdd", "1",
-		"--hdd-ops", strconv.Itoa(ioOpsBase * ioLoad),
-	)
-	log.Printf("io load amount: %d\n", ioLoad)
+	filename := "/tmp/ben_base_stress_" + strconv.FormatUint(rand.Uint64(), 10)
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	w := bufio.NewWriter(f)
+	totalBytes, currentBytes := 0, 0
+	for i := 0; i < ioOpsBase * ioLoad; i++ {
+		// Write 1 KiB to buffer at one time
+		n, err := w.WriteString(strings.Repeat("contents", 128))
+		if err != nil {
+			panic(err)
+		}
+
+		totalBytes += n
+		currentBytes += n
+		// Flush buffered data every 512 KiB
+		if currentBytes >= 1024 * 512 {
+			err = w.Flush()
+			if err != nil {
+				panic(err)
+			}
+			err = f.Sync()
+			if err != nil {
+				panic(err)
+			}
+
+			currentBytes -= 1024 * 512
+		}
+	}
+
+	err = w.Flush()
+	if err != nil {
+		panic(err)
+	}
+	err = f.Sync()
+	if err != nil {
+		panic(err)
+	}
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Remove(filename)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("io load amount: %d, total bytes written: %d\n", ioLoad, ioOpsBase * 8 * 128)
 }
